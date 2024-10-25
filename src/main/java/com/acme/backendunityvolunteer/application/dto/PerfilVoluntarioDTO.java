7a9990b481284da0bd91308bf5d4de9de52167d6
Anthonnela <U20211C403@upc.edu.pe>
2024-10-25 10:33:34 -0500

    first commit

---
+++ src/main/java/com/acme/backendunityvolunteer/application/dto/PerfilVoluntarioDTO.java
@@ -0,0 +1,15 @@
+package com.acme.backendunityvolunteer.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PerfilVoluntarioDTO {
+    private Long id;
+    private Long usuarioId;
+    private String intereses;
+    private String experiencia;
+    private String disponibilidad;
+    private String puntuacionPromedio;
+}
